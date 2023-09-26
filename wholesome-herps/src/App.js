4c4f26ecff89f4cd055f327ddda9d1e2305817fa
ChemicalModel <michaelmcleod133@gmail.com>
2023-09-26 10:46:43 -0400

    added routing for pages and some context for test

--- wholesome-herps/src/App.js
+++ wholesome-herps/src/App.js
@@ -1,13 +1,26 @@
-import logo from './logo.svg';
+// src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/Homepage';
-
+import Header from './components/Header';
+import Footer from './components/Footer';
+import AboutMe from './pages/AboutMe';
+import FAQ from './pages/FAQ';
 
 function App() {
   return (
-    <div className="App">
-      <HomePage />
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path="/AboutMe" element={<AboutMe />} />
+          <Route path="/FAQ" element={<FAQ />} />
+          <Route path="/Homepage" element={<HomePage />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
---
+++ wholesome-herps/src/pages/FAQ.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function FAQ() {
+  return (
+    <div>
+      <h1>FAQ Content</h1>
+    </div>
+  );
+}
+
+export default FAQ;
